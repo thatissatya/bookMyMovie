7b527736b7b3c812d146842045b602092b0d545c
Satya Prakash <satyaprakash2436@gmail.com>
2021-01-30 20:18:04 +0530

    V3.0

--- utils/viewShow.py
+++ utils/viewShow.py
@@ -1,2 +1,24 @@
 
+def displayAvailableShow():
+
+    showlist = ''
+    try :
+
+        fptr = open('showlist.txt', 'r')
+        showlist = fptr.readlines()
+        fptr.close()
+
+    except :
+        print("Error! Database access failed ...")
+    
+    showlist = showlist.split()
+
+    sno = 0
+    print('S.no ' +'  '  + 'Movie Name ' + '  ' 'Time ' +'  '  + 'Availabe Seats ' + '  '  )
+    for shows in showlist :
+
+        details = shows.split('-')
+        showname  = str(sno)  +'   '  + details[0] + '  ' + details[1] + details[2]
+        print(showname)
+
 
